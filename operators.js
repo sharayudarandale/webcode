8adac82663975ea9c255049d88c81224d3f0fc5a
Sharayu_Darandale <168580698+sharayudarandale@users.noreply.github.com>
2024-10-12 13:56:27 +0530

    Add files via upload

---
+++ operators.js
@@ -0,0 +1,46 @@
+// Arithmetic Operators
+let a=5;
+let b=2;
+console.log("a+b=",a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+
+// Modulus operator %
+console.log("a%b=",a%b);
+
+
+// exponention
+console.log("a**b=",a**b); //5^2=25
+
+//Unary operator
+
+a=a+1;
+console.log(a);
+a++;
+console.log(a);
+
+// Assignment Operator
+x=4
+x+=4;
+console.log("x+=",x);
+x**=2;
+console.log("x**=",x); //4^2=64
+
+// Comparison operator
+console.log(a==b);
+let c="3";
+let d=5;
+console.log(a===b); //=== cheks data types also
+
+// Logical 
+let y=6;
+let z=5;
+let cond1=y>z;
+let cond2=z==6;
+console.log("cond1 && cond2" ,cond1 && cond2); //logican and
+console.log(!(y==z)); //not
+
+
+
+
