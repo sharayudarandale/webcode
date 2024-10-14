d5a728dad1ba08dc4b47de84b8868ee910fbfa1c
Sharayu_Darandale <168580698+sharayudarandale@users.noreply.github.com>
2024-10-14 16:10:51 +0530

    Add files via upload

---
+++ loops.js
@@ -0,0 +1,10 @@
+// for loop
+for(let i=1;i<=3;i++){
+    console.log("Apna college");
+}
+// calculate sum of 1 to 10
+let sum=0;
+for(let j=1;j<=10;j++){
+    sum=sum+j;
+}
+console.log("sum=",sum);
